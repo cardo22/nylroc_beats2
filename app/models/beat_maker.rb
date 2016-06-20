343024cec3fa5c10d08bcbf5f13e9ec0154a0043
Ricardo Clarke <wolfpack3371@gmail.com>
2016-06-19 23:56:42 -0400

    did validations for beat_maker, riddim, and user

--- app/models/beat_maker.rb
+++ app/models/beat_maker.rb
@@ -1,5 +1,8 @@
 class BeatMaker < ActiveRecord::Base
 	has_many :riddims
 
+	validates :first_name, presence: true
+  	validates :last_name, presence: true
+  	validates :birthday, presence: true
 	validates :user_name, presence: true
 end
--- app/models/riddim.rb
+++ app/models/riddim.rb
@@ -1,3 +1,6 @@
 class Riddim < ActiveRecord::Base
 	belongs_to :beat_makers
+
+	validates :name, presence: true
+	validates :duration, presence: true
 end
