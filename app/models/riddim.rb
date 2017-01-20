060295ce1c485ef7b0701fac4ef423a3fca8a736
Ricardo Clarke <wolfpack3371@gmail.com>
2017-01-20 14:59:21 -0500

    added devise validation to users

--- app/models/riddim.rb
+++ app/models/riddim.rb
@@ -1,5 +1,6 @@
 class Riddim < ActiveRecord::Base
 	belongs_to :beat_makers
+	belongs_to :users
 
 	validates :name, presence: true
 	validates :duration, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+	has_many :riddims
+
+	validates :first_name, presence: true
+  	validates :last_name, presence: true
+  	validates :birthday, presence: true
+	validates :user_name, presence: true
+	validates :email, presence: true
+	validates :phone_number, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
