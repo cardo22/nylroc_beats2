1d970d94ec359d056661940858f5983ce0140f56
Ricardo Clarke <wolfpack3371@gmail.com>
2017-01-21 17:32:11 -0500

    fixing seeds bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-beat_maker1 = BeatMaker.create(first_name: "Ricardo", last_name: "Clarke", user_name: "Nylroc Beats", birthday: Date.new(1993, 8, 3))
+beat_maker1 = User.create(first_name: "Ricardo", last_name: "Clarke", user_name: "Nylroc Beats", birthday: Date.new(1993, 8, 3), email: "nylroc_beats@gmail.com", phone_number: 4079298235)
 riddim1 = Riddim.create(name: "Trap Beat", duration: 5, snippet: "Output 1-2", purchases: 10, leases: 30, beat_maker_id: 1)
